4122b3cb51c6d91aef9631925ad8dd52eadec8ad
Artur Didur <didur.art@gmail.com>
2024-11-22 12:07:01 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Review from "./components/Review";
+
+const App = () => {
+  return (
+    <main>
+      <h2 className="title">Reviews</h2>
+      <Review />
+    </main>
+  );
+};
+export default App;
